f9609d47b6f9dca55e52e47de01c664b5c8b204e
Daniel Sauder <daniel.sauder@web.de>
2018-02-10 12:04:58 +0100

    add build script

---
+++ build/build_win32_msgbox_inject_shellcode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash          
+# example script for injecting shellcode into a process
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win32.sh
+# simple messagebox 
+msfvenom -p windows/messagebox -b '\x00' -f c -a x86 --platform Windows > sc.txt
+# call make_avet, compule shellcode into the executable
+./make_bfg -f sc.txt -i shellcode -P
+# compile 
+$win32_compiler -o bfg.exe bfg.c
+# cleanup
+#rm sc.txt && echo "" > defs.h
