305b9393adb7ad42ce8e75611cc7d0b567a1d722
Daniel Sauder <daniel.sauder@web.de>
2018-02-13 22:04:13 +0100

    update hello.c & co

--- payloads/hello.c
+++ payloads/hello.c
@@ -1,11 +1,10 @@
-// # wine gcc -m32 -o hello.exe hello.c -mwindows
+// dll: wine gcc -o hello.dll hello.c -shared -mwindows
+// exe: wine gcc -o hello.exe hello.c -mwindows
+// rundll32 hello.dll,main
+#include <windows.h>
 
-#include <windows.h> 
-
-int WINAPI 
-WinMain (HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpCmdLine, int nShowCmd) 
-{ 
-  MessageBoxW (NULL, L"Hello World!", L"hello", MB_OK | MB_ICONINFORMATION); 
-  return 0; 
+void main (void)
+{
+	MessageBoxW (NULL, L"Hello World!", L"hello", MB_OK | MB_ICONINFORMATION); 
 }
 
