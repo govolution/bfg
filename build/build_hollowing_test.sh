94df253ec265d70ce6c17f06303eb35f509724b2
Florian Saager <florian.saager@rub.de>
2018-02-14 15:11:55 +0100

    Added test build script for process hollowing

---
+++ build/build_hollowing_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash          
+# simple example script for 32 bit process hollowing test, injecting hello world message box
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win32.sh
+# build 32 bit hello executable
+$win32_compiler -o payloads/hello.exe payloads/hello.c
+# assume empty defs.h, cleanup
+echo "" > defs.h
+# call make_bfg, compile payload into executable
+./make_bfg -H payloads/hello.exe
+# compile 
+$win32_compiler -o bfg.exe bfg.c
+# cleanup
+echo "" > defs.h
